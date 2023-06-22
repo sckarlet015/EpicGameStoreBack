9311659c89566492927b4105f661dccb26fca9a3
sckarlet015 <sckarlet015@gmail.com>
2023-06-21 23:53:04 -0600

    Actualizacion codgo de modelos

--- src/models/Cliente.js
+++ src/models/Cliente.js
@@ -1,17 +1,16 @@
 const { DataTypes } = require('sequelize');
-const sequelize = require('../config/database'); // Configuración de la base de datos
 
-const Cliente = sequelize.define('cliente', {
-  nombre: {
-    type: DataTypes.STRING,
-    allowNull: false
-  },
-  email: {
-    type: DataTypes.STRING,
-    allowNull: false,
-    unique: true
-  },
-  // Otros campos del cliente (dirección, número de teléfono, etc.)
-});
-
-module.exports = Cliente;
+module.exports = (sequelize) => {
+  sequelize.define('cliente', {
+    nombre: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true
+    },
+    // Otros campos del cliente (dirección, número de teléfono, etc.)
+  })
+}
