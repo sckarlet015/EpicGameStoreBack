16734f1758aee0e903f79fa381af8d61eb0c0d18
sckarlet015 <sckarlet015@gmail.com>
2023-06-21 23:28:45 -0600

    codeo del archivo index.js (server)

---
+++ index.js
@@ -0,0 +1,13 @@
+const server = require('./src/app.js');
+const {
+  conn
+} = require('./src/db.js');
+
+// Syncing all the models at once.
+conn.sync({
+  force: true
+}).then(async () => {
+  server.listen(3001, () => {
+    console.log('Servidor en el puerto 3001'); // eslint-disable-line no-console
+  });
+});
