99e54125f58d0b6a11af364f08345216a54cdcc4
Gilberto castaño <gilberto.castano@outlook.es>
2023-07-14 23:43:54 -0500

    ignore

--- src/models/Users.js
+++ src/models/Users.js
@@ -36,6 +36,7 @@ module.exports = (sequelize) => {
     },
     role: {
       type: DataTypes.ENUM('cliente', 'vendedor', 'admin'),
+      defaultValue: `cliente`,
       allowNull: false,
     }
   }, {
